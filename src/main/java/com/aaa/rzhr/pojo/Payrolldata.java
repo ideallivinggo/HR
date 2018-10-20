67f7806d25dee1b0db139aa723ea1ff7ae0e8b2f
31830 <318306985@qq.com>
2018-10-20 14:38:05 +0800

    10-20徐代宋金鹏

---
+++ src/main/java/com/aaa/rzhr/pojo/Payrolldata.java
@@ -0,0 +1,97 @@
+package com.aaa.rzhr.pojo;
+
+
+public class Payrolldata {
+
+  private long payrid;
+  private double payrbase;
+  private double payrdays;
+  private double payrhour;
+  private double payrpacific;
+  private double payrweekend;
+  private double payrlegal;
+  private String payname;
+  private double payjixiao;
+
+
+  public long getPayrid() {
+    return payrid;
+  }
+
+  public void setPayrid(long payrid) {
+    this.payrid = payrid;
+  }
+
+///--
+  public double getPayrbase() {
+    return payrbase;
+  }
+
+  public void setPayrbase(double payrbase) {
+    this.payrbase = payrbase;
+  }
+
+
+  public double getPayrdays() {
+    return payrdays;
+  }
+
+  public void setPayrdays(double payrdays) {
+    this.payrdays = payrdays;
+  }
+
+
+  public double getPayrhour() {
+    return payrhour;
+  }
+
+  public void setPayrhour(double payrhour) {
+    this.payrhour = payrhour;
+  }
+
+
+  public double getPayrpacific() {
+    return payrpacific;
+  }
+
+  public void setPayrpacific(double payrpacific) {
+    this.payrpacific = payrpacific;
+  }
+
+
+  public double getPayrweekend() {
+    return payrweekend;
+  }
+
+  public void setPayrweekend(double payrweekend) {
+    this.payrweekend = payrweekend;
+  }
+
+
+  public double getPayrlegal() {
+    return payrlegal;
+  }
+
+  public void setPayrlegal(double payrlegal) {
+    this.payrlegal = payrlegal;
+  }
+
+
+  public String getPayname() {
+    return payname;
+  }
+
+  public void setPayname(String payname) {
+    this.payname = payname;
+  }
+
+
+  public double getPayjixiao() {
+    return payjixiao;
+  }
+
+  public void setPayjixiao(double payjixiao) {
+    this.payjixiao = payjixiao;
+  }
+
+}
