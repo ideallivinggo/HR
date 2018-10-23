d515fb2232502e4651458b1ab722cc90ba16ceb4
yqx <3177078782@qq.com>
2018-10-23 14:09:38 +0800

    10-23于庆贤第三次

---
+++ src/main/java/com/aaa/rzhr/pojo/Att_Statistics.java
@@ -0,0 +1,37 @@
+package com.aaa.rzhr.pojo;
+
+
+public class Att_Statistics {
+
+  private long staid;
+  private String stagsname;
+  private String statjmonth;
+
+
+  public long getStaid() {
+    return staid;
+  }
+
+  public void setStaid(long staid) {
+    this.staid = staid;
+  }
+
+
+  public String getStagsname() {
+    return stagsname;
+  }
+
+  public void setStagsname(String stagsname) {
+    this.stagsname = stagsname;
+  }
+
+
+  public String getStatjmonth() {
+    return statjmonth;
+  }
+
+  public void setStatjmonth(String statjmonth) {
+    this.statjmonth = statjmonth;
+  }
+
+}
---
+++ src/main/java/com/aaa/rzhr/pojo/Att_Statisticsxq.java
@@ -0,0 +1,107 @@
+package com.aaa.rzhr.pojo;
+
+
+public class Att_Statisticsxq {
+
+  private long worid;
+  private long staid;
+  private long empid;
+  private double worday;
+  private double worbelate;
+  private double worpaffairs;
+  private double worweekwork;
+  private double worpswork;
+  private double worhdaywork;
+  private double worklatesum;
+
+
+  public long getWorid() {
+    return worid;
+  }
+
+  public void setWorid(long worid) {
+    this.worid = worid;
+  }
+
+
+  public long getStaid() {
+    return staid;
+  }
+
+  public void setStaid(long staid) {
+    this.staid = staid;
+  }
+
+
+  public long getEmpid() {
+    return empid;
+  }
+
+  public void setEmpid(long empid) {
+    this.empid = empid;
+  }
+
+
+  public double getWorday() {
+    return worday;
+  }
+
+  public void setWorday(double worday) {
+    this.worday = worday;
+  }
+
+
+  public double getWorbelate() {
+    return worbelate;
+  }
+
+  public void setWorbelate(double worbelate) {
+    this.worbelate = worbelate;
+  }
+
+
+  public double getWorpaffairs() {
+    return worpaffairs;
+  }
+
+  public void setWorpaffairs(double worpaffairs) {
+    this.worpaffairs = worpaffairs;
+  }
+
+
+  public double getWorweekwork() {
+    return worweekwork;
+  }
+
+  public void setWorweekwork(double worweekwork) {
+    this.worweekwork = worweekwork;
+  }
+
+
+  public double getWorpswork() {
+    return worpswork;
+  }
+
+  public void setWorpswork(double worpswork) {
+    this.worpswork = worpswork;
+  }
+
+
+  public double getWorhdaywork() {
+    return worhdaywork;
+  }
+
+  public void setWorhdaywork(double worhdaywork) {
+    this.worhdaywork = worhdaywork;
+  }
+
+
+  public double getWorklatesum() {
+    return worklatesum;
+  }
+
+  public void setWorklatesum(double worklatesum) {
+    this.worklatesum = worklatesum;
+  }
+
+}
