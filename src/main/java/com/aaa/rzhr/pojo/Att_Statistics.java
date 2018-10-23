a2f1fe8fbb2023682b9df23bf8e9b320de64fdd0
songjp <745667575@qq.com>
2018-10-23 14:19:56 +0800

    2018-10-23 Sjp2

---
+++ src/main/java/com/aaa/rzhr/pojo/Att_Statistics.java
@@ -0,0 +1,37 @@
+package com.aaa.rzhr.pojo;
+
+//ssssssssssssssssssss
+public class Att_Statistics {
+
+  private Integer staid;
+  private String stagsname;
+  private String statjmonth;
+
+
+  public Integer getStaid() {
+    return staid;
+  }
+
+  public void setStaid(Integer staid) {
+    this.staid = staid;
+  }
+
+
+  public String getStagsname() {
+    return stagsname;
+  }
+
+  public void setStagsname(String stagsname) {
+    this.stagsname = stagsname;
+  }
+
+
+  public String getStatjmonth() {
+    return statjmonth;
+  }
+
+  public void setStatjmonth(String statjmonth) {
+    this.statjmonth = statjmonth;
+  }
+
+}
